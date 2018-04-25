554382725c5c0f7767b09098ec127dd6963ba0c6
Francisco Rafart <francisco.rafart@gmail.com>
2018-04-25 07:42:30 -0400

    progress

--- euler27.js
+++ euler27.js
@@ -1,5 +1,3 @@
-let cuadratic = (n,a,b)=>{return Math.pow(n,2)+(a*n)+b}
-
 let is_prime = n => {
   if (n==2) return true
   if (n<2) return false
@@ -21,5 +19,38 @@ let give_array = (i,n) =>{
   return res
 }
 
-let a = b = give_array(-1000,1000);
-let products = a.map(as => b.map(bs => bs*as))
+let pows = give_array(0,1000).map(n => Math.pow(n,2)) //store in memory for efficiency
+
+let cuadratic = (n,a,b)=>{return pows[n]+(a*n)+b} //use stored variable for efficiency
+let odds = x => {return x%2!=0}
+// const negatives = arr => {return arr.map(e => e*-1)}
+
+let a = b = give_array(-1000,1000).filter(odds);
+
+//for each pair of coeficients, go through n and count the primes. Return the value of a and b that has more primes.
+let n = give_array(0,1000);
+largest = (obj1, obj2) =>{return obj1.prime_count > obj2.prime_count? obj1:obj2}
+// let res = a.map(as => b.map(bs => n.map(ns => cuadratic(ns,as,bs))))
+
+let res = a.map(as =>
+              b.map(
+                bs => {
+                  let newObj = {}
+                  newObj['a'] = as
+                  newObj['b'] = bs
+                  newObj['prime_count'] = n.filter(ns => {return is_prime(cuadratic(ns,as,bs))}).length
+                  console.log(newObj)
+                  return newObj
+                }
+              ).reduce(largest)
+            ).reduce(largest)
+
+let result = res['a']*res['b']
+console.log(result)
+
+
+//test reduce pattern
+
+// let numbers = [1,2,3,4,5,6,7,8,
+//Number of primes
+// console.log(numbers.filter(x => {return is_prime(x)}).length)
